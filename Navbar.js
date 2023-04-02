be34b2a809f9f03b8265081cf802b5922187af71
AjaleighS <129632233+AjaleighS@users.noreply.github.com>
2023-04-02 04:54:16 -0400

    Update Navbar.js

--- Navbar.js
+++ Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar() {
     {name:"TALKS", link:"/"},
   ];
   return (
-    <div className="shadow-md w-full fixed top-0 left-0">
+    <div className="sticky top-0 shadow-md w-full fixed top-0 left-0">
       <div className="md:flex items-center justify-between bg-white py-4 md:px-10 px-7">
         <div className="font-bold text-2xl flex items-center text-gray-800">
         <span className="text-3xl text-indigo-600 mr-1 pt-2"></span>
